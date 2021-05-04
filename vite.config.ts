5d191538116fb2f1139a53ad4e3e419627c7d7fb
yaskevich <alyaxey.yaskevich@gmail.com>
2021-05-05 00:10:34 +0300

    configurate modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+ plugins: [vue()],
+ resolve: {
+	alias: {
+	  '@': require('path').resolve(__dirname, 'src')
+	}
+  },
+ server: {
+	port: 7777,
+	host: "localhost",
+	https: false,
+	open: false,
+	proxy: {
+		"/api": {
+			target: "http://localhost:3061",
+			changeOrigin: true,
+			ws:true,
+			logLevel: 'debug'
+		}
+	}
+ }
 })
