cef564fa481c1ebb588b006ae1b03685591be3e7
yaskevich <alyaxey.yaskevich@gmail.com>
2022-01-16 13:04:45 +0300

    enable PrimeFlex for app [client]

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,6 +5,7 @@ import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import InputNumber from 'primevue/inputnumber';
