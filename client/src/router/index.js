760fbd4d491a0a5edd461e73f0265ae074451c6d
yaskevich <alyaxey.yaskevich@gmail.com>
2022-02-13 23:31:26 +0300

    update router [client]

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Deadlines from '../views/Deadlines.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/deadlines',
+    name: 'Deadlines',
+    component: Deadlines
   },
   {
     path: '/about',
