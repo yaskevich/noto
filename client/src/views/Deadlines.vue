4e0019dad8c699d17ec0d88d1cc503bac165487c
yaskevich <alyaxey.yaskevich@gmail.com>
2022-02-18 23:46:22 +0300

    render posts in Deadlines [client]

--- client/src/views/Deadlines.vue
+++ client/src/views/Deadlines.vue
@@ -1,5 +1,18 @@
 <template>
-  hello
+
+  <div class="text-center" style="text-align:center;max-width:400px;margin: auto">
+    <div v-for="(item, key) in posts"
+         class="shadow-11 item p-2 mb-3 text-left"
+         style="border: 1px dashed black;margin:0 auto;"
+         :key="key"
+         :title="renderDate(item?.time)">
+      <div v-if="item.date" style="color:red;font-weight:bold;">{{renderDate(item?.date)}}</div>
+      <span class="title">{{item.title}}</span>
+
+      <div v-if="item.content" v-html="html(item.content)" class="content"></div>
+    </div>
+  </div>
+
 </template>
 
 <script setup lang="ts">
@@ -7,6 +20,8 @@
   import { ref, reactive, onBeforeMount } from 'vue';
   import axios from 'axios';
   import { generateHTML } from '@tiptap/core';
+  import StarterKit from '@tiptap/starter-kit';
+  import Link from '@tiptap/extension-link';
 
   interface IItem {
     title: string;
@@ -23,8 +38,8 @@
   onBeforeMount(async () => {
     const { data } = await axios.get('/api/deadlines');
     console.log(data);
-    Object.assign(posts, data?.posts?.sort((a, b) => b.time - a.time).reverse());
-
+    Object.assign(posts, data?.sort((a, b) => b.time - a.time).reverse());
+    console.log(posts);
   });
 
   const renderDate = (x: any) => {
@@ -37,9 +52,5 @@
         .slice(0, -3);
     }
   };
-
-
-
-
 
 </script>
