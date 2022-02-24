ad18684eac30cc4d6de6980dd00190a473770ba5
yaskevich <alyaxey.yaskevich@gmail.com>
2022-02-24 23:41:46 +0300

    update navigation menu in App [client]

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/deadlines">Deadlines</router-link> |
+    <router-link to="/persons">Persons</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
