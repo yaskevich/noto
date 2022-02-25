eaa2d9017ff074772f3396097189e8c0a1ad09f0
yaskevich <alyaxey.yaskevich@gmail.com>
2022-02-25 23:41:23 +0300

    fetch data from database in Persons [client]

--- client/src/views/Persons.vue
+++ client/src/views/Persons.vue
@@ -8,5 +8,14 @@
 
 <script setup lang="ts">
 
+  import { ref, reactive, onBeforeMount } from 'vue';
+  import axios from 'axios';
+
+  const persons = reactive([]);
+  onBeforeMount(async () => {
+    const { data } = await axios.get('/api/persons');
+    console.log(data);
+    Object.assign(persons, data);
+  });
 
 </script>
