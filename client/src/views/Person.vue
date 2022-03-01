4f8a2577e59f0c39bd665821c8d70ff03f6d4a05
yaskevich <alyaxey.yaskevich@gmail.com>
2022-03-01 23:28:18 +0300

    get data from database in Person [client]

--- client/src/views/Person.vue
+++ client/src/views/Person.vue
@@ -1,12 +1,29 @@
 <template>
 
   <div class="text-center" style="text-align:center;max-width:400px;margin: auto">
-    test
+    {{person}}
   </div>
 
 </template>
 
 <script setup lang="ts">
 
+  import { ref, reactive, onBeforeMount } from 'vue';
+  import axios from 'axios';
+  import { useRoute } from 'vue-router';
+  const person = reactive({});
+
+  const vuerouter = useRoute();
+  const id = vuerouter.params.id;
+  console.log('person id', id);
+  // import router from '../router';
+
+  onBeforeMount(async () => {
+    const config = {};
+    config['params'] = { id: id };
+    const { data } = await axios.get('/api/person', config);
+    console.log(data);
+    Object.assign(person, data);
+  });
 
 </script>
