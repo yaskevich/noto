966250be6553c7dea83d765dcf94dfd76d301012
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-12-11 21:51:35 +0100

    handle category change in Note

--- client/src/views/Note.vue
+++ client/src/views/Note.vue
@@ -1,28 +1,15 @@
 <template>
-  <div class="text-center" style="text-align: center; max-width: 400px; margin: auto">
-    <InputText
-      id="name"
-      aria-describedby="name-help"
-      type="text"
-      v-model="note.title"
-      class="p-d-block p-mx-auto"
+  <div class="text-center" style="text-align: center;">
+    <InputText id="name" aria-describedby="name-help" type="text" v-model="note.title" class="p-d-block p-mx-auto"
       autocomplete="off" />
     <Button label="Save" @click="handleClick" />
-
     <div class="mt-2 mb-2">
+      <Dropdown v-model="selectedCat" :options="cats" optionLabel="title" optionValue="id" placeholder="Select a City"
+        class="w-full md:w-14rem" />
       <!-- <label for="time24">Date time</label> -->
-      <Calendar
-        id="time24"
-        v-model="note.alarm"
-        :showTime="true"
-        :showIcon="true"
-        :showButtonBar="true"
-        :hideOnDateTimeSelect="true"
-        :touchUI="true"
-        :showOnFocus="false"
-        dateFormat="yy.mm.dd" />
+      <Calendar id="time24" v-model="note.alarm" :showTime="true" :showIcon="true" :showButtonBar="true"
+        :hideOnDateTimeSelect="true" :touchUI="true" :showOnFocus="false" dateFormat="yy.mm.dd" />
     </div>
-
     <editor-content :editor="editor" class="editor" ref="contentRef" />
   </div>
 </template>
@@ -37,28 +24,37 @@ import helpers from '../helpers';
 const contentRef = ref<HTMLDivElement>();
 const editor = helpers.setupEditor('');
 const note = reactive({}) as IPost;
+const cats = reactive([] as Array<ICat>);
 const vuerouter = useRoute();
 const id = vuerouter.params.id;
-console.log('note id', id);
-// import router from '../router';
+const selectedCat = ref(1);
 
 onBeforeMount(async () => {
   if (id) {
     const config = { params: { id: id } };
     const { data } = await axios.get('/api/note', config);
-    data.alarm = new Date(data.alarm);
+    if (data?.alarm) {
+      data.alarm = new Date(data.alarm);
+    }
     Object.assign(note, data);
     const ttInstance = (contentRef.value as any).editor;
-    console.log(data.content);
+    // console.log(data.content);
     ttInstance.commands.setContent(JSON.parse(data.content));
+    selectedCat.value = note.cat;
+    const res = await axios.get('/api/cats');
+    Object.assign(
+      cats,
+      res.data
+    );
   }
 });
 
 const handleClick = async () => {
   const datum = { ...note };
-  datum.content = editor.value?.getJSON() || '';
-  console.log('note', datum);
+  datum.cat = selectedCat.value;
+  datum.content = JSON.stringify(editor.value?.getJSON()) || '';
+  // console.log('note', datum);
   const { data } = await axios.post('/api/note', datum);
-  console.log('post', data);
+  console.log('post', data.id);
 };
 </script>
