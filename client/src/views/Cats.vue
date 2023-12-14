429a1acd5349b73ce8e4189e5e0e7dad603fb35d
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-12-14 15:14:38 +0100

    create component

---
+++ client/src/views/Cats.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="text-center">
+        <div v-for="(item, index) in cats" :key="index" class="p-1" style="max-width:300px;margin: 0 auto">
+            <InputGroup class="">
+                <InputText type="text" v-model="item.title" />
+                <Button icon="pi pi-check" />
+            </InputGroup>
+        </div>
+    </div>
+</template>
+    
+<script setup lang="ts">
+import { reactive, onBeforeMount } from 'vue';
+import axios from 'axios';
+import Unit from './Unit.vue';
+
+const cats = reactive([] as Array<ICat>);
+
+onBeforeMount(async () => {
+    const res = await axios.get('/api/cats');
+    Object.assign(
+        cats,
+        res.data
+    );
+});
+</script>
+    
