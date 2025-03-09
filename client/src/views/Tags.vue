610777fddc0d550162c4b91ee7c107b20312b567
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-03-09 22:49:02 +0100

    create component

---
+++ client/src/views/Tags.vue
@@ -0,0 +1,21 @@
+<template>
+<div v-for="item in tags" style="font-weight: bold; margin-bottom:1rem; color:red">
+    {{ item.title }}
+</div>
+</template>
+
+<script setup lang="ts">
+import { reactive, onBeforeMount } from 'vue';
+import axios from 'axios';
+
+const tags = reactive([] as Array<ICat>);
+
+onBeforeMount(async () => {
+    const res = await axios.get('/api/tags');
+    Object.assign(
+        tags,
+        res.data
+    );
+});
+</script>
+    
