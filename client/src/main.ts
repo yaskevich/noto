638b5e10396a70c298bf5df50f1ac8cd7ced2e23
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-03-22 20:23:18 +0100

    load new theme

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,68 +1,18 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router.ts';
 import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
-import InputNumber from 'primevue/inputnumber';
-import Dialog from 'primevue/dialog';
-import SelectButton from 'primevue/selectbutton';
-import Divider from 'primevue/divider';
-import Toast from 'primevue/toast';
-import AutoComplete from 'primevue/autocomplete';
-import Inplace from 'primevue/inplace';
-import MultiSelect from 'primevue/multiselect';
-import Tag from 'primevue/tag';
-import InputSwitch from 'primevue/inputswitch';
-import Panel from 'primevue/panel';
-import TabView from 'primevue/tabview';
-import TabPanel from 'primevue/tabpanel';
-import Dropdown from 'primevue/dropdown';
-import Badge from 'primevue/badge';
-import ScrollTop from 'primevue/scrolltop';
-import SplitButton from 'primevue/splitbutton';
-import Calendar from 'primevue/calendar';
-import Checkbox from 'primevue/checkbox';
-import RadioButton from 'primevue/radiobutton';
-import InputGroup from 'primevue/inputgroup';
-
 import ToastService from 'primevue/toastservice';
-
-// import "@fontsource/fira-sans-extra-condensed" // Defaults to weight 400 with normal variant.
-// import "@fontsource/fira-sans-extra-condensed/400-italic.css" // Italic variant.
-// import "@fontsource/fira-sans-extra-condensed/700.css" // Bold variant.
-// import "@fontsource/fira-sans-extra-condensed/700-italic.css" // Bold italic variant.
-// import "@fontsource/fira-sans-extra-condensed/900.css" // Black variant.
+import 'primeflex/primeflex.css';
+import Aura from '@primeuix/themes/aura';
+import 'primeicons/primeicons.css';
 
 createApp(App)
-  .use(PrimeVue)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura
+    }
+  })
   .use(router)
   .use(ToastService)
-  .component('Dialog', Dialog)
-  .component('Button', Button)
-  .component('SplitButton', SplitButton)
-  .component('InputText', InputText)
-  .component('InputNumber', InputNumber)
-  .component('ScrollTop', ScrollTop)
-  .component('Badge', Badge)
-  .component('Dropdown', Dropdown)
-  .component('TabView', TabView)
-  .component('TabPanel', TabPanel)
-  .component('Panel', Panel)
-  .component('SelectButton', SelectButton)
-  .component('InputSwitch', InputSwitch)
-  .component('MultiSelect', MultiSelect)
-  .component('Inplace', Inplace)
-  .component('AutoComplete', AutoComplete)
-  .component('Toast', Toast)
-  .component('Divider', Divider)
-  .component('Tag', Tag)
-  .component('Calendar', Calendar)
-  .component('Checkbox', Checkbox)
-  .component('RadioButton', RadioButton)
-  .component('InputGroup', InputGroup)
   .mount('#app');
