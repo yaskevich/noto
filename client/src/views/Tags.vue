f364f6b1808a8ec280638d04f107f76da214038e
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-06-26 23:09:22 +0200

    handle adding new tags

--- client/src/views/Tags.vue
+++ client/src/views/Tags.vue
@@ -1,21 +1,26 @@
 <template>
-<div v-for="item in tags" style="font-weight: bold; margin-bottom:1rem; color:red">
-    {{ item.title }}
-</div>
+    <div class="mb-4">
+        <InputText type="text" v-model="title" /><Button label="Save" @click="addTag" />
+    </div>
+    <div v-for="item in tags" style="font-weight: bold; margin-bottom:1rem; color:red">
+        {{ item.title }}
+    </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, onBeforeMount } from 'vue';
-import axios from 'axios';
+import { onBeforeMount, ref } from 'vue';
+import h from '../helpers';
 
-const tags = reactive([] as Array<ICat>);
+const title = ref('');
+const tags = ref([] as Array<ICat>);
+const addTag = async () => {
+    const result = await h.save('tags', { title: title.value });
+    // console.log(title.value, result);
+    if (result.changes) {
+        tags.value.push({ id: result.lastID, title: title.value })
+        title.value = '';
+    }
+};
 
-onBeforeMount(async () => {
-    const res = await axios.get('/api/tags');
-    Object.assign(
-        tags,
-        res.data
-    );
-});
+onBeforeMount(async () => tags.value = await h.get('tags'));
 </script>
-    
