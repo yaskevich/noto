d2cde954805bac23860330c39384cbef0cbe6558
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-08-12 17:11:41 +0200

    replace deprecated component

--- client/src/views/Person.vue
+++ client/src/views/Person.vue
@@ -1,26 +1,13 @@
 <template>
   <div class="text-center">
-    <InputText
-      id="name"
-      aria-describedby="name-help"
-      type="text"
-      v-model="person.name"
-      class="p-d-block p-mx-auto"
+    <InputText id="name" aria-describedby="name-help" type="text" v-model="person.name" class="p-d-block p-mx-auto"
       autocomplete="off" />
     <Button label="Save" @click="handleClick" />
 
     <div class="mt-2 mb-2">
       <!-- <label for="time24">Date time</label> -->
-      <Calendar
-        id="time24"
-        v-model="person.bday"
-        :showTime="false"
-        :showIcon="true"
-        :showButtonBar="true"
-        :hideOnDateTimeSelect="true"
-        :touchUI="true"
-        :showOnFocus="false"
-        dateFormat="yy.mm.dd" />
+      <DatePicker id="time24" v-model="person.bday" :showTime="false" :showIcon="true" :showButtonBar="true"
+        :hideOnDateTimeSelect="true" :touchUI="true" :showOnFocus="false" dateFormat="yy.mm.dd" />
     </div>
 
     <editor-content :editor="editor" class="editor" ref="contentRef" />
@@ -46,8 +33,6 @@ onBeforeMount(async () => {
   if (id) {
     const config = { params: { id: id } };
     const { data } = await axios.get('/api/person', config);
-    // console.log("database", data);
-    data.bday = data.bday.replaceAll('-', '.');
     Object.assign(person, data);
     const ttInstance = (contentRef.value as any).editor;
     ttInstance.commands.setContent(JSON.parse(data.content));
