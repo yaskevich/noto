28d764611b6752338cb922c9a022025ae08eb984
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-08-25 16:32:08 +0200

    render dialog with emoji

--- client/src/views/Tags.vue
+++ client/src/views/Tags.vue
@@ -4,13 +4,31 @@
     </div>
     <div v-for="(item, index) in tags" style="font-weight: bold; margin-bottom:1rem; color:red">
         <Button label="Delete" severity="danger" @click="deleteTag(item, index)" />
+        <Button :label="item?.emoji || '?'" severity="secondary" @click="renderDialog(item)" />
         <InputText type="text" v-model="item.title" /> <Button label="Rename" @click="renameTag(item, index)" />
     </div>
+
+    <Dialog v-model:visible="visible" modal :header="'Update emoji: ' + selectedEmoji" :style="{ width: '25rem' }">
+        <span class="text-surface-500 dark:text-surface-400 block"></span>
+        <div class="flex items-center gap-4 mb-4">
+            <EmojiPicker :native="true" @select="onSelectEmoji" disable-skin-tones />
+        </div>
+        <div class="flex justify-end gap-2">
+            <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
+            <Button type="button" label="Save" @click="visible = false"></Button>
+        </div>
+    </Dialog>
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount, ref } from 'vue';
 import h from '../helpers';
+import EmojiPicker from 'vue3-emoji-picker';
+import 'vue3-emoji-picker/css';
+
+const selected = ref<ICat>();
+const visible = ref(false);
+const selectedEmoji = ref<string>('');
 
 const title = ref('');
 const tags = ref([] as Array<ICat>);
@@ -29,6 +47,22 @@ const addTag = async () => {
         tags.value.push({ id: result.lastID, title: title.value })
         title.value = '';
     }
+};
+
+const onSelectEmoji = async (emoji: any) => {
+    console.log('selected', emoji.i);
+    selectedEmoji.value = emoji.i;
+    if (selected.value) {
+        selected.value.emoji = emoji.i;
+        console.log(selected.value);
+        console.log(emoji.i);
+    }
+};
+
+const renderDialog = (item: ICat) => {
+    selected.value = item;
+    selectedEmoji.value = item?.emoji || '';
+    visible.value = true;
 };
 
 const deleteTag = async (entry: ICat, num: number) => {
