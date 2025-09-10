dd13ad193ffa7e121f12308753dfdde8533131e0
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-09-10 17:11:52 +0200

    implement creating categories

--- client/src/views/Cats.vue
+++ client/src/views/Cats.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="text-center">
+        <div class="mb-4">
+            <InputText type="text" v-model="title" /><Button label="Add" @click="addItem" />
+        </div>
+
         <div v-for="(item, index) in cats" :key="index" class="p-1" style="max-width:300px;margin: 0 auto">
             <InputGroup class="">
                 <InputText type="text" v-model="item.title" />
@@ -13,6 +17,7 @@
 import { ref, onBeforeMount } from 'vue';
 import h from '../helpers';
 
+const title = ref('');
 const cats = ref([] as Array<ICat>);
 
 const saveItem = async (item: ICat) => {
@@ -20,7 +25,14 @@ const saveItem = async (item: ICat) => {
     console.log(result);
 };
 
-onBeforeMount(async () => {
-    cats.value = await h.get('cats');
-});
+const addItem = async () => {
+    const result = await h.save('cats', { title: title.value });
+    // console.log(title.value, result);
+    if (result.changes) {
+        cats.value.push({ id: result.lastID, title: title.value })
+        title.value = '';
+    }
+};
+
+onBeforeMount(async () => cats.value = await h.get('cats'));
 </script>
