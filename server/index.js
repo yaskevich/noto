ec5731555600c05eb6386fe4b3b55245497e1bb2
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-09-15 17:21:41 +0200

    show entries from future if date is now

--- server/index.js
+++ server/index.js
@@ -260,12 +260,10 @@ app.get('/api/deadlines', async (req, res) => {
 app.get('/api/dated', async (req, res) => {
   const days = Number(req.query.days) || 30;
   const from = req.query.from;
-  console.log(days, from);
-const when = req.query.from || 'now';
-const sql = `SELECT * FROM posts WHERE (stamped = 1 OR alarm is not null) AND deleted IS NOT TRUE AND wholeday IS TRUE AND alarm > datetime('${when}', '-${days} day') AND alarm < datetime('${when}')`;
-console.log(sql);
-const posts = await db.all(sql);
-
+  const when = from || 'now';
+  const ext = from ? `AND alarm < datetime('${when}')` : '';
+  const sql = `SELECT * FROM posts WHERE deleted IS NOT TRUE AND wholeday IS TRUE AND alarm > datetime('${when}', '-${days} day') ${ext}`;
+  const posts = await db.all(sql);
   res.json(posts);
 });
 
